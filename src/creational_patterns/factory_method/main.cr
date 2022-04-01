35065fa06f9296ee1360dba5591adf4183a9e7af
takaakit <takaaki.teshima.dev@gmail.com>
2022-04-02 07:48:40 +0900

    Fixed typo

--- src/creational_patterns/factory_method/main.cr
+++ src/creational_patterns/factory_method/main.cr
@@ -1,6 +1,6 @@
 require "./credit_card/credit_card_factory"
 
-# The subject is a factory to make credit cards. The Factory defines how to create an credit card,
+# The subject is a factory to make credit cards. The Factory defines how to create a credit card,
 # but the actual credit card is created by the CreditCardFactory.
 # The "createProduct()" is called a Factory Method, and it is responsible for manufacturing an object.
 

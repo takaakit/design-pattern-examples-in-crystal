91dd18db48f566999e7444a74e693996392b5277
takaakit <takaaki.teshima.dev@gmail.com>
2022-07-30 15:48:22 +0900

    Add comments

--- src/structural_patterns/proxy/proxy_printer.cr
+++ src/structural_patterns/proxy/proxy_printer.cr
@@ -4,6 +4,7 @@ require "./real_printer"
 
 # ˄
 
+# ProxyPrinter forwards requests to RealPrinter when appropriate.
 class ProxyPrinter < Printer
   # ˅
 
@@ -36,15 +37,18 @@ class ProxyPrinter < Printer
     if @real.nil? == false
       @real.not_nil!.change_name(name)
     end
+
     @current_name = name
     # ˄
   end
 
   def output(content : String)
     # ˅
+    # Check to see if the the RealPrinter had been created, create it if necessary.
     if @real.nil?
       @real = RealPrinter.new(@current_name)
     end
+
     @real.not_nil!.output(content)
     # ˄
   end
